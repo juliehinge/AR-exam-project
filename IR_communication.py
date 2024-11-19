10b31c6c55acf25afe76abc65520208c4378b84d
henni3 <henni_95@hotmail.com>
2024-11-19 14:43:16 +0100

    avoiderController should work

--- IR_communication.py
+++ IR_communication.py
@@ -3,6 +3,7 @@
 # TODO correct color scheme
 
 from tdmclient import ClientAsync
+import time
 
 seeker_program = """
 var send_interval = 200  # time in milliseconds
@@ -40,7 +41,22 @@ onevent prox.comm
 
 class AvoiderController:
     def __init__(self):
+        self.is_tagged = False
+        self.in_grey_area = False
+        self.last_message_time = None
 
+        # Set the LED lights on the robot
+        def led_state(node, color):
+            node.v.leds.top = color
+            node.v.leds.bottom.left = color
+            node.v.leds.bottom.right = color
+
+        # Running the Thymio robot
+        def run_motor(node, left, right):
+            node.v.motor.left.target = left  
+            node.v.motor.right.target = right   
+        
+        # Obstacle avoidance bevahior
         def behaviorOA(prox_values, reflected_values):
             """
             Obstacle avoidance behavior function.
@@ -65,17 +81,16 @@ class AvoiderController:
             else:
                 return 100, 100
 
-
-        def avoid_black(reflected_values, node):
-
+        # Detect which area the robot is in
+        def area_detection(reflected_values, node):
+            #Detect black lines
             if reflected_values[0] < 200 or reflected_values[1] < 200:
                 return -100, -100
 
+            #Detects grey area
             elif (reflected_values[0] > 200 and reflected_values[0] < 500) or (reflected_values[1] > 200 and reflected_values[1] < 500):
-                node.v.leds.top = [0, 0, 32]
-                node.v.leds.bottom.left = [0, 0, 32]
-                node.v.leds.bottom.right = [0, 0, 32]
-
+                led_state(node, [0, 32, 0])
+                self.in_grey_area = True
                 return 0, 0
             else:
                 return 100, 100
@@ -104,30 +119,57 @@ class AvoiderController:
                     await node.wait_for_variables({"prox.horizontal"})
                     print("Thymio started successfully!")
 
+                    # Enable the proximity communication 
+                    node.v.prox.comm.enable = 1
+                    # Set time interval on 200ms
+                    node.v.timer.period[0] = 200
+                    # Set initial LED state
+                    led_state(node, [0, 0, 32])
+                    node.flush() #send the initial state to Thymio
+                    
+                    # Transmits the message "2" when robot is not in the grey area and it has been
+                    # away from the grey area for a minimum of 5 sec.
+                    async def on_timer0():
+                        if (not self.in_grey_area) and (self.last_message_time is None or time.time() - self.last_message_time >= 5):
+                            node.v.prox.comm.tx = 2
+                            node.flush()
+                            self.last_message_time = None #reset last message time
+
+                    # When Thymio receives a message "1" it is tagged, stops and turns purple
+                    async def on_prox_comm():
+                        message = node.v.prox.comm.rx
+                        print(f"message from Thymio: {message}")
+                        if message == 1:
+                            led_state(node, [32, 0, 32])    # turns purple
+                            run_motor(node, 0, 0)           # stop motor
+                            node.flush()
+                            self.is_tagged = True      # robot is tagged and should terminate
+                        elif (message == 2) and self.in_grey_area:
+                            led_state(node, [0, 0, 32])    # turns purple
+                            run_motor(node, 100, 100)           # stop motor
+                            node.flush()
+                            self.last_message_time = time.time()
+
                     while True:
                         # get the values of the proximity sensors
                         prox_values = node.v.prox.horizontal
                         reflected_values = node.v.prox.ground.reflected
                         
-
+                        await on_timer0()
+                        await on_prox_comm()
                         """
                         Get the value of the message received from the other Thymio
                         the value is 0 if no message has been received and 
                         gets set to a new value when a message is received" 
                         """
-
-                        message = node.v.prox.comm.rx
-                        print(f"message from Thymio: {message}")
-
-                        if sum(prox_values) > 20000:
+                        # if the robot is blocked or it has been tagged, the program should terminate
+                        if (sum(prox_values) > 20000) or self.is_tagged:
                             break
 
-                       # speeds = behaviorOA(prox_values, reflected_values)
+                        #speeds = behaviorOA(prox_values, reflected_values)
                         
-                        speeds = avoid_black(reflected_values, node)
-
-                        node.v.motor.left.target = speeds[1]
-                        node.v.motor.right.target = speeds[0]
+                        speeds = area_detection(reflected_values, node)
+                        run_motor(node, speeds[1], speeds[0])
 
                         node.flush()  # Send the set commands to the robot.
 
